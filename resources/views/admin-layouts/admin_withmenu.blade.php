3a62ffb4e42dd3f6ddc1ff65db7fa87db332dddc
Kamornpisit <kamornpisit03@gmail.com>
2021-04-19 23:27:25 +0700

    add admin login

--- resources/views/admin-layouts/admin_withmenu.blade.php
+++ resources/views/admin-layouts/admin_withmenu.blade.php
@@ -172,7 +172,7 @@
                             @guest
                             @else
                                 <li class="nav-item">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                    <a href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
                                                                                                                                       document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
