f2b8731b1dab322935ab9cc24baee47c08be0a41
Kamornpisit <kamornpisit03@gmail.com>
2021-04-22 17:00:23 +0700

    link controller

--- app/Http/Controllers/MainTitleController.php
+++ app/Http/Controllers/MainTitleController.php
@@ -58,10 +58,27 @@ class MainTitleController extends Controller
         foreach($main as $image)
         {
         $output .= '
-        <div class="col-md-3" style="margin-bottom:16px;" align="center">
-                    <label for="" class="col-sm-2 col-form-label text-left">รูปที่ '.$number.'</label>
-                    <img src="'.asset($image->image_path).'" class="img-thumbnail" width="175" height="175" style="height:175px;" />
-                    <button type="button" class="btn btn-link remove_image" id="'.$image->id.'">Remove</button>
+                <div class="col-md-3" style="margin-bottom:16px;" align="center">
+                    <div>
+                        <label for="" class="col-sm-2 col-form-label text-left">รูปที่ '.$number.'</label>
+                        <img src="'.asset($image->image_path).'" class="img-thumbnail" width="175" height="175" style="height:175px;" />
+                    </div>
+                    <div>
+                    <div class="form-group row mt-3">
+                    <label for="meta_title" class="col-sm-2 col-form-label text-right">Link</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" placeholder="link" id="link-'.$image->id.'" name="link-'.$image->id.'">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6 text-right">
+                            <button type="button" class="btn btn-primary save_link" value="'.$image->link.'" id="save_link-'.$image->id.'">save</button>
+                        </div>
+                        <div class="col-sm-6">
+                        <button type="button" class="btn btn-primary remove_image" id="'.$image->id.'">Remove</button>
+                        </div>
+                    </div>
+                    </div>
                 </div>
         ';
         $number ++;
